426349c08c7c110ff6d45814f9aff8dde8045d9f
Algorithms <142917637+Algo1337@users.noreply.github.com>
2026-01-15 06:56:46 -0600

    update

--- dick.c
+++
@@ -1,9 +0,0 @@
-void _test(register long rdi, register long rsi, register long rdx, register long ebx)
-{
-
-}
-
-void _start()
-{
-	_test(60,0,0,0);
-}
---
+++ src/asm/x86_64_mem.asm
@@ -0,0 +1,54 @@
+section .bss
+	buffer resb 1024
+	buff resb 1024
+
+section .data
+
+section .text
+	global _start
+
+; @label: strcpy
+; @arguments:
+;	RDI = char *
+;	AL = char
+; 	RDX = size
+;
+; @return: void
+mem_set:
+	xor r10, r10
+
+	.mem_set_loop:
+		cmp rdx, r10
+		je .exit_mem_set
+
+		mov byte [rdi + r10], al
+		inc r10
+
+		jmp .mem_set_loop
+
+	.exit_mem_set:
+		ret
+
+; @label: mem_cpy
+; @arguments:
+; 	RDI = char *
+; 	RSI = char *
+; 	RDX = size
+;
+; @return(RAX): idx as success or -1 as mismatch in
+mem_cpy:
+	xor r10, r10
+
+	.mem_cpy_loop:
+		cmp rdx, r10
+		je .exit_success_mem_cpy
+
+		mov al, byte [rsi + r10]
+		mov byte [rdi + r10], al
+
+		inc r10
+		jmp .mem_cpy_loop
+
+	.exit_success_mem_cpy:
+		mov rax, rdx
+		ret
