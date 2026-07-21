a58068104943ee1ca12102e10216f6ae98fde087
Algo1337 <intensesec@gmail.com>
2026-07-21 13:07:08 -0500

    test moved

--- tests/thread.c
+++ tests/thread.c
@@ -1,26 +1,20 @@
 #include <fsl.h>
 
-void thread_func(string q)
+void test_fn(void *x)
 {
-    println("HI");
-    if(!q)
-        fsl_panic("NULL argument");
-
-    _printf("ptr: %p\n", q);
-    __syscall__(1, (long)q, 6, -1, -1, -1, _SYS_WRITE);
-
-    _printf("ptr: %p\n", q);
+    register string v asm("r13");
+    _printf("received: %p\n", v);
+    println(v);
     __syscall__(0, 0, 0, 0, 0, 0, _SYS_EXIT);
 }
 
 int entry()
 {
-    string str = allocate(0, 10);
-    str_append(str, "hello\n");
-    _printf("ptr: %p\n", str);
-	___start_thread((void *)thread_func, "hello\n");
+    string n = allocate(0, 10);
+    str_append(n, "dick\n");
+    run_thread(test_fn, n);
+
     _sleep(2);
-	println("Done");
+
     return 0;
 }
-
